1f16d0942ccfc0c44d53f4abd1695619427a6670
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-02 14:15:11 +0900

    Update ContainerComponent.vue

--- src/components/ContainerComponent.vue
+++ src/components/ContainerComponent.vue
@@ -9,6 +9,25 @@
     -->
     <!-- <Post/>
     <Post/> -->
+
+    <!-- 저번시간에 만든거 -->
+    <!-- <Post :게시물="게시물[i]" v-for="(a, i) in 게시물" :key="i" /> -->
+
+    <!-- 필터선택페이지 -->
+    <div class="upload-image"></div>
+    <div class="filters">
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div class="upload-image"></div>
+    <div class="write">
+      <textarea class="write-box">write!</textarea>
+    </div>
   </div>
 </template>
 
@@ -27,5 +46,46 @@ export default {
 </script>
 
 <style>
-
+.upload-image{
+	width: 100%;
+	height: 450px;
+	background: cornflowerblue;
+	background-size : cover;
+}
+.filters{
+	overflow-x:scroll;
+	white-space: nowrap;
+}
+.filter-1 {
+	width: 100px;
+	height: 100px;
+	background-color: cornflowerblue;
+	margin: 10px 10px 10px auto;
+	padding: 8px;
+	display: inline-block;
+	color : white;
+	background-size: cover;
+}
+.filters::-webkit-scrollbar {
+	height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+	background: #f1f1f1; 
+}
+.filters::-webkit-scrollbar-thumb {
+	background: #888; 
+	border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+	background: #555; 
+}
+.write-box {
+	border: none;
+	width: 90%;
+	height: 100px;
+	padding: 15px;
+	margin: auto;
+	display: block;
+	outline: none;
+}
 </style>

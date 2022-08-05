31dbe07a24afa89da47765332ae1d6adf169304a
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-05 22:44:21 +0900

    Add files via upload

---
+++ src/components/FilterBoxComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="필터" class="filter-item" :style="`background-image: url(${이미지}) `"></div> 
+  <!-- <div :class="필터 + ` filter-item`" :style="`background-image: url(${이미지}) `"></div>  -->
+  <!-- <div :class="`${필터} filter-item`" :style="`background-image: url(${이미지}) `"></div>  -->
+</template>
+
+<script>
+export default {
+  name: 'FilterBoxComponent', // 이름 아무렇게나 작명 가능
+  props: {
+    이미지: String,
+    필터: String,
+  }
+}
+</script>
+
+<style>
+.filter-item {
+  width: 100px;
+  height: 100px;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color : white;
+  background-size: cover;
+  background-position : center;
+}
+</style>

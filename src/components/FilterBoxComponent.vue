b2e2ef3580b5c1d23af8bc4adf9a4dff180de4ac
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-05 23:04:40 +0900

    Update FilterBoxComponent.vue
    slot 사용

--- src/components/FilterBoxComponent.vue
+++ src/components/FilterBoxComponent.vue
@@ -1,5 +1,13 @@
 <template>
-  <div :class="필터" class="filter-item" :style="`background-image: url(${이미지}) `"></div> 
+  <div :class="필터" class="filter-item" :style="`background-image: url(${이미지}) `">
+    <!-- 
+      slot으로 부모 -> 자식 데이터 전송 가능 
+      1. 자식은 구멍 뚫기
+      2. 부모 컴포넌트에서 <컴포넌트> 태그 사이에 데이터 넣기
+      slot은 HTML 태그 안에 데이터바인딩할 때만 slot을 사용한다.
+      -->
+    <slot></slot> <!-- 1. 구멍 뚫기 -->
+  </div> 
   <!-- <div :class="필터 + ` filter-item`" :style="`background-image: url(${이미지}) `"></div>  -->
   <!-- <div :class="`${필터} filter-item`" :style="`background-image: url(${이미지}) `"></div>  -->
 </template>

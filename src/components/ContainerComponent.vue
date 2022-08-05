9734987c89bc0cd0ccc2aa36209b036916bf84d3
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-05 22:43:25 +0900

    Update ContainerComponent.vue
    cssgram

--- src/components/ContainerComponent.vue
+++ src/components/ContainerComponent.vue
@@ -19,11 +19,10 @@
     <div v-if="step == 1">
       <div class="upload-image" :style="`background-image: url(${이미지url})`"></div>
       <div class="filters">
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
+        <FilterBox 
+        :필터="필터" 
+        :이미지="이미지url" 
+        v-for="(필터, i) in 필터들" :key="i">{{ 필터 }}</FilterBox>
       </div>
     </div>
 
@@ -39,9 +38,17 @@
 
 <script>
 import Post from './PostComponent.vue'
+import FilterBox from './FilterBoxComponent.vue'
 
 export default {
   name: 'ContainerComponent',
+  data() {
+    return {
+      필터들: [ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson", 
+"inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua", 
+"reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"],
+    }
+  },
   props: {
     이미지url: String,
     step: Number,
@@ -49,6 +56,7 @@ export default {
   },
   components: {
     Post,
+    FilterBox,
   }
 }
 </script>

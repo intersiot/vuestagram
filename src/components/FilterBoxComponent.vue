aba8365086ec4ae347bc51d5038cf09e7f25b4eb
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-06 17:39:09 +0900

    Update FilterBoxComponent.vue

--- src/components/FilterBoxComponent.vue
+++ src/components/FilterBoxComponent.vue
@@ -2,12 +2,13 @@
   <div :class="필터" class="filter-item" :style="`background-image: url(${이미지}) `">
     <!-- 
       slot으로 부모 -> 자식 데이터 전송 가능 
-      1. 자식은 구멍 뚫기
+      1. 자식은 구멍 뚫기 -> <slot></slot>으로
       2. 부모 컴포넌트에서 <컴포넌트> 태그 사이에 데이터 넣기
       slot은 HTML 태그 안에 데이터바인딩할 때만 slot을 사용한다.
       -->
-    <slot></slot> <!-- 1. 구멍 뚫기 -->
-    <!-- mitt로 데이터 전송하는 법
+    <slot></slot>
+    <!-- 
+      mitt로 데이터 전송하는 법
       1. this.emitter.emit() 으로 발사하고
       2. this.emitter.on() 으로 수신하면 됨 -> 수신하는 코드는 mounted 안에 작성하는 게 관습적임
      -->

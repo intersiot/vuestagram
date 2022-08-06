140de978938974bfeef4091ed81869eb260fb62e
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-06 17:35:21 +0900

    Update main.js
    mitt 셋팅

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import mitt from 'mitt' // mitt 라이브러리 셋팅
+let emitter = mitt();
+let app = createApp(App)
+app.config.globalProperties.emitter = emitter;
 
-createApp(App).mount('#app')
+app.mount('#app')

bb06b2de955a1b592405f90d43db0044e6e0bca9
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-09 19:24:20 +0900

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 import { createStore } from 'vuex'
+import post from './assets/post.js'
+let data = post();
 
 // 버튼을 누르면 state => 'park'로 바꿔보자.
 // 1. store.js에 state 수정방법 정의
@@ -9,19 +11,22 @@ const store = createStore({
     return { // 데이터 보관하고 싶으면 여기에 하셈 -> 데이터말고 state라고 할거임
       name: 'kim',
       age: 20,
-      likes: 30, // 좋아요 갯수
+      likes: data.likes, // 좋아요 갯수 -> post를 import하는 방법이 뭔가 잘못됨..
     }
   },
   mutations: { // state 수정방법 정의하는 곳임
-    이름변경(state) { // state 파라미터를 추가 -> 위에 있는 state 오브젝트를 뜻함
-      state.name = 'park'
+    UpLike(state) {
+      state.likes++
     },
-    나이먹기(state) {
-      state.age++
-    },
-    나이십씩먹기(state, data) {
-      state.age += data
-    }
+    // 나이먹기(state) {
+    //   state.age++
+    // },
+    // 나이십씩먹기(state, data) {
+    //   state.age += data
+    // }
+    // 이름변경(state) { // state 파라미터를 추가 -> 위에 있는 state 오브젝트를 뜻함
+    //   state.name = 'park'
+    // },
   }
 })
 

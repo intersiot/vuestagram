38c8cc12b779e2db593fc7e8a2d369a278d43657
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-09 22:17:18 +0900

    Update store.js
    actions

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 import { createStore } from 'vuex'
-// import post from './assets/post.js'
+import post from './assets/post'
 
 // 버튼을 누르면 state => 'park'로 바꿔보자.
 // 1. store.js에 state 수정방법 정의
@@ -8,13 +9,17 @@ import { createStore } from 'vuex'
 const store = createStore({
   state(){
     return { // 데이터 보관하고 싶으면 여기에 하셈 -> 데이터말고 state라고 할거임
-      name: 'kim',
-      age: 20,
+      게시물: post,
       likes: 30,
       liked: false,
+      more: {}, // 서버에서 더보기 게시물 가져와서 여기에 저장해보자
+      더보기: 0,
     }
   },
-  mutations: { // state 수정방법 정의하는 곳임
+  mutations: { // state 수정방법 정의하는 곳임, state 변경은 무조건 mutations가 함.
+    setMore(state, data) { // 두번째 파라미터는 setMore를 쓸 때마다 데이터를 넣을 수 있다.
+      state.more = data
+    },
     UpLike(state) {
       if (state.liked == false) {
         state.likes++;
@@ -24,16 +29,17 @@ const store = createStore({
         state.liked = false;
       }
     },
-    // 나이먹기(state) {
-    //   state.age++
-    // },
-    // 나이십씩먹기(state, data) {
-    //   state.age += data
-    // }
-    // 이름변경(state) { // state 파라미터를 추가 -> 위에 있는 state 오브젝트를 뜻함
-    //   state.name = 'park'
-    // },
-  }
+  },
+  actions: { // ajax하는 곳 또는 오래걸리는 작업들
+    // ajax 요청하는 법
+    // 예시1. 더보기 게시물 ajax 요청하려면?
+    getData(context) { // commit을 쓸때는 파라미터로 context를 추가해줘야 함. 이때 context는 store임.
+      axios.get(`https://codingapple1.github.io/vue/more${this.더보기}.json`).then((a) => {
+        console.log(a.data);
+        context.commit('setMore', a.data) // setMore함수를 동작시켜주세요. 이때 commit함
+      })
+    },
+  },
 })
 
 export default store

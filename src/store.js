5d7fd5cfa9efb8485666d700a3a34d2a6dc1d907
intersiot <75784873+intersiot@users.noreply.github.com>
2022-08-10 15:24:31 +0900

    Update store.js
    좋아요 이제 다됨

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createStore } from 'vuex'
-import post from './assets/post'
+import post from './assets/post.js'
 
 // 버튼을 누르면 state => 'park'로 바꿔보자.
 // 1. store.js에 state 수정방법 정의
@@ -9,24 +9,30 @@ import post from './assets/post'
 const store = createStore({
   state(){
     return { // 데이터 보관하고 싶으면 여기에 하셈 -> 데이터말고 state라고 할거임
-      게시물: post,
-      likes: 30,
-      liked: false,
+      name: 'kim',
+      age: 20,
+      post: post,
+      // likes: 30,
+      // liked: false,
       more: {}, // 서버에서 더보기 게시물 가져와서 여기에 저장해보자
       더보기: 0,
     }
   },
   mutations: { // state 수정방법 정의하는 곳임, state 변경은 무조건 mutations가 함.
     setMore(state, data) { // 두번째 파라미터는 setMore를 쓸 때마다 데이터를 넣을 수 있다.
-      state.more = data
+      state.more = data;
     },
-    UpLike(state) {
-      if (state.liked == false) {
-        state.likes++;
-        state.liked = true;
+    UpLike(state, i) {
+      if (state.post[i].liked== false) {
+        state.post[i].likes++;
+        state.post[i].liked = true;
+        console.log(state.post[i].likes)
+        console.log(state.post[i].liked)
       } else {
-        state.likes--;
-        state.liked = false;
+        state.post[i].likes--;
+        state.post[i].liked = false;
+        console.log(state.post[i].likes)
+        console.log(state.post[i].liked)
       }
     },
   },
